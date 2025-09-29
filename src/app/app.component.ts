cdf26fd4a9f0aec5490ff2e245164078298715c8
Sofiane Zairi <szairi2011@gmail.com>
2025-09-29 17:19:47 +0100

    Polished and improved design of the products and the product-card screens to make it more professional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './shared/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Back 2 Buy';
+  constructor(private themeService: ThemeService) {
+    this.themeService.init();
+  }
 }
